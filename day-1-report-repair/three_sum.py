c8c02c5616991eb8a3d37e2e8e9fcf57cf8dd1e2
Rajit Banerjee <rajit.banerjee@ucdconnect.ie>
2020-12-01 21:16:38 +0000

    oh damn

--- day-1-report-repair/three_sum.py
+++ day-1-report-repair/three_sum.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
 
 def threeSum(nums: list) -> str:
-    seen = {}
     for n in nums:
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if 2020 - (nums[i] + nums[j]) not in seen:
-                    seen[n] = 2020 - (nums[i] + nums[j])
-                else:
-                    return (f'{n} * {nums[i]} * {nums[j]} = {n*nums[i]*nums[j]}')
+        for m in nums:
+            p = 2020 - (n + m)
+            if p in nums:
+                return (f'{n} * {m} * {p} = {n*m*p}')
+
 
 with open('input.txt') as f:
     nums = [int(n) for n in f.readlines()]
