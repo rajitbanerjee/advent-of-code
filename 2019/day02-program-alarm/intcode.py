bc740b0f29f4e4421360a91f2ea583949738b2fe
Rajit Banerjee <rajit.banerjee@ucdconnect.ie>
2020-12-07 13:54:17 +0000

    speed, nested binary search

--- 2019/day02-program-alarm/intcode.py
+++ 2019/day02-program-alarm/intcode.py
@@ -5,7 +5,7 @@ def readInput() -> list:
         return list(map(int, f.read().strip().split(',')))
 
 
-def simulate(inA: int = 12, inB: int = 2) -> int:
+def simulate(inA: int, inB: int) -> int:
     program = readInput()
     program[1], program[2] = inA, inB
     run(program)
@@ -25,15 +25,19 @@ def run(program: list) -> list:
     return program
 
 
-def nounVerb() -> int:
+def nounVerb(target: int) -> int:
     program = readInput()
-    noun, verb = findInputs(program, target=19690720)
+    noun, verb = findInputs(program, target)
     return 100 * noun + verb
 
 
 def findInputs(program: list, target: int) -> (int, int):
-    for a in range(100):
+    left, right = 0, 99
+    # binary search for input A
+    while left <= right:
+        a = left + (right - left) // 2
         lo, hi = 0, 99
+        # binary search for input B
         while lo <= hi:
             b = lo + (hi - lo) // 2
             res = simulate(a, b)
@@ -43,9 +47,13 @@ def findInputs(program: list, target: int) -> (int, int):
                 lo = b + 1
             else:
                 hi = b - 1
+        if res < target:
+            left = a + 1
+        else:
+            right = a - 1
     return (0, 0)
 
 
 if __name__ == '__main__':
-    print(f"Part 1 = {simulate()}")
-    print(f"Part 2 = {nounVerb()}")
+    print(f"Part 1 = {simulate(inA=12, inB=2)}")
+    print(f"Part 2 = {nounVerb(target=19690720)}")
