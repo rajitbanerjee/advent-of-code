7fdc029c78c6e8e73606131bf21b47a0e3b208ca
Rajit Banerjee <rajit.banerjee@ucdconnect.ie>
2020-12-11 12:58:33 +0000

    clean up functions

--- 2020/day11-seating-system/seating.py
+++ 2020/day11-seating-system/seating.py
@@ -3,86 +3,16 @@
 from copy import deepcopy
 
 
-def isOccupied(i: int, j: int, layout: list) -> int:
-    if isValid(i, j, layout):
-        if layout[i][j] == '#':
-            return 1
-    return 0
+def simulate(layout: list, tolerance: int = 4,
+             only_first: bool = False) -> int:
+    while True:
+        new_layout = update(layout, tolerance, only_first)
+        if isIdentical(new_layout, layout):
+            return countOccupied(new_layout)
+        layout[:] = new_layout
 
 
-def isValid(i: int, j: int, layout: list) -> bool:
-    rows, cols = len(layout), len(layout[0])
-    return 0 <= i < rows and 0 <= j < cols
-
-
-def countOccupiedAround(i: int, j: int, layout: list,
-                        only_first: bool = False) -> int:
-    if only_first:
-        around = getFirstEachDir(i, j, layout)
-    else:
-        around = [(i-1, j-1), (i-1, j), (i-1, j+1), (i, j-1), (i, j+1),
-                  (i+1, j-1), (i+1, j), (i+1, j+1)]
-    return sum([isOccupied(a, b, layout) for a, b in around])
-
-
-def getFirstEachDir(i: int, j: int, layout: list) -> list:
-    around = []
-    # right
-    x, y = i, j+1
-    while isValid(x, y, layout) and layout[x][y] == '.':
-        y += 1
-    around.append((x, y))
-
-    # left
-    x, y = i, j-1
-    while isValid(x, y, layout) and layout[x][y] == '.':
-        y -= 1
-    around.append((x, y))
-
-    # top
-    x, y = i-1, j
-    while isValid(x, y, layout) and layout[x][y] == '.':
-        x -= 1
-    around.append((x, y))
-
-    # bottom
-    x, y = i+1, j
-    while isValid(x, y, layout) and layout[x][y] == '.':
-        x += 1
-    around.append((x, y))
-
-    # top left diagonal
-    x, y = i-1, j-1
-    while isValid(x, y, layout) and layout[x][y] == '.':
-        x -= 1
-        y -= 1
-    around.append((x, y))
-
-    # top right diagonal
-    x, y = i-1, j+1
-    while isValid(x, y, layout) and layout[x][y] == '.':
-        x -= 1
-        y += 1
-    around.append((x, y))
-
-    # bottom left diagonal
-    x, y = i+1, j-1
-    while isValid(x, y, layout) and layout[x][y] == '.':
-        x += 1
-        y -= 1
-    around.append((x, y))
-
-    # bottom right diagonal
-    x, y = i+1, j+1
-    while isValid(x, y, layout) and layout[x][y] == '.':
-        x += 1
-        y += 1
-    around.append((x, y))
-
-    return around
-
-
-def update(layout: list, tol: int = 4, only_first: bool = False) -> list:
+def update(layout: list, tolerance: int, only_first: bool = False) -> list:
     new_layout = deepcopy(layout)
     for i in range(len(layout)):
         for j in range(len(layout[0])):
@@ -90,9 +20,52 @@ def update(layout: list, tol: int = 4, only_first: bool = False) -> list:
                     countOccupiedAround(i, j, layout, only_first) == 0:
                 new_layout[i][j] = '#'
             elif layout[i][j] == '#' and \
-                    countOccupiedAround(i, j, layout, only_first) >= tol:
+                    countOccupiedAround(i, j, layout, only_first) >= tolerance:
                 new_layout[i][j] = 'L'
     return new_layout
+
+
+def countOccupiedAround(i: int, j: int, layout: list,
+                        only_first: bool = False) -> int:
+    if not only_first:
+        # problem part 1
+        around = [(i-1, j-1), (i-1, j), (i-1, j+1), (i, j-1), (i, j+1),
+                  (i+1, j-1), (i+1, j), (i+1, j+1)]
+    else:
+        # problem part 2
+        around = getFirstEachDir(i, j, layout)
+    return sum([isOccupied(a, b, layout) for a, b in around])
+
+
+def getFirstEachDir(i: int, j: int, layout: list) -> list:
+    around = []
+    directions = [(0, 1), (0, -1), (-1, 0), (1, 0),
+                  (-1, -1), (-1, 1), (1, -1), (1, 1)]
+
+    for dx, dy in directions:
+        x, y = i+dx, j+dy
+        # keep moving in current direction until seat is found
+        while isValid(x, y, layout) and layout[x][y] == '.':
+            x += dx
+            y += dy
+        around.append((x, y))
+
+    return around
+
+
+def isValid(i: int, j: int, layout: list) -> bool:
+    rows, cols = len(layout), len(layout[0])
+    return 0 <= i < rows and 0 <= j < cols
+
+
+def isOccupied(i: int, j: int, layout: list) -> bool:
+    return isValid(i, j, layout) and layout[i][j] == '#'
+
+
+def countOccupied(layout: list) -> int:
+    rows, cols = len(layout), len(layout[0])
+    around = [(i, j) for j in range(cols) for i in range(rows)]
+    return sum([isOccupied(a, b, layout) for a, b in around])
 
 
 def isIdentical(a: list, b: list) -> bool:
@@ -103,23 +76,9 @@ def isIdentical(a: list, b: list) -> bool:
     return True
 
 
-def countOccupied(layout: list) -> int:
-    rows, cols = len(layout), len(layout[0])
-    around = [(i, j) for j in range(cols) for i in range(rows)]
-    return sum([isOccupied(a, b, layout) for a, b in around])
-
-
-def simulate(layout: list, tol: int, only_first: bool = False) -> list:
-    while True:
-        new_layout = update(layout, tol, only_first)
-        if isIdentical(new_layout, layout):
-            return countOccupied(new_layout)
-        layout[:] = new_layout
-
-
 if __name__ == '__main__':
     with open('day11.in') as f:
         layout = [list(line.strip()) for line in f.readlines()]
 
-    print(f"Part 1 = {simulate(deepcopy(layout), tol=4)}")
-    print(f"Part 2 = {simulate(deepcopy(layout), tol=5, only_first=True)}")
+    print(f"Part 1 = {simulate(deepcopy(layout))}")
+    print(f"Part 2 = {simulate(deepcopy(layout), tolerance=5, only_first=True)}")
