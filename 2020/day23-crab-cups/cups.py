b3527061d24e05e901b28cae367212887fcc1856
Rajit Banerjee <rajit.banerjee@ucdconnect.ie>
2020-12-23 23:17:01 +0000

    day 23 part 2; switch to linked lists approach

--- 2020/day23-crab-cups/cups.py
+++ 2020/day23-crab-cups/cups.py
@@ -3,56 +3,93 @@
 from typing import List, Tuple
 
 
+class ListNode:
+    def __init__(self, label):
+        self.label = label
+        self.next = None
+
+
 def main() -> None:
     with open('day23.in') as f:
-        cups = list(map(int, list(f.read().strip())))
+        labels = list(map(int, list(f.read().strip())))
 
-    print(f"Part 1: {playGame(cups[:])}")
-    cups.extend(i for i in range(max(cups) + 1, 10**6 + 1))
-    print(f"Part 2: {playGame(cups[:], n_moves=10**7)}")
+    print(f"Part 1: {playGame(labels)}")
+    print(f"Part 2: {playGame(labels, mistranslation=True, num_moves=10**7)}")
 
 
-def playGame(cups: List[int], n_moves: int = 100) -> str:
-    curr = cups[0]
-    for _ in range(n_moves):
-        cups, curr = move(cups, curr)
-    if n_moves <= 100:
-        return getOrder(cups)
+def playGame(labels: List[int], mistranslation: bool = False,
+             num_moves: int = 100) -> str:
+    first_cup, cups = arrangeCups(labels, mistranslation)
+    current_cup = makeMoves(first_cup, cups, num_moves).next
+
+    if not mistranslation:
+        # combine all labels after and up to (wrap around) cup 1
+        labelling = []
+        while current_cup.label != 1:
+            labelling.append(str(current_cup.label))
+            current_cup = current_cup.next
+        return ''.join(labelling)
     else:
-        one = cups.index(1)
-        n = len(cups)
-        print(cups[(one + 1) % n], cups[(one + 2) % n])
-        return ""
+        # product of two cups after cup 1
+        return current_cup.label * current_cup.next.label
 
 
-def move(cups: List[int], curr: int) -> Tuple[List[int], int]:
-    dest = curr - 1
-    picked_cups = pickThree((cups.index(curr) + 1) % len(cups), cups)
-    curr = cups[(cups.index(curr) + 1) % len(cups)]
+def arrangeCups(labels: List[int],
+                mistranslation: bool = False) -> Tuple[ListNode, dict]:
+    cups, last_cup = {}, None
+    for label in labels:
+        current_cup = ListNode(label)
+        if last_cup:
+            last_cup.next = current_cup
+        last_cup = current_cup
+        cups[label] = current_cup
 
-    while True:
-        if dest != 0 and dest not in picked_cups:
-            break
-        dest = (dest - 1) % (max(cups) + 1)
+    if mistranslation:
+        for label in range(10, 10**6 + 1):
+            current_cup = ListNode(label)
+            if last_cup:
+                last_cup.next = current_cup
+            last_cup = current_cup
+            cups[label] = current_cup
 
-    new_cups = cups[:]
-    for cup in picked_cups[::-1]:
-        new_cups.insert(new_cups.index(dest) + 1, cup)
+    first_cup = cups[labels[0]]
+    if last_cup:
+        last_cup.next = first_cup
 
-    return new_cups, curr
-
-
-def pickThree(index: int, cups: List[int]) -> List[int]:
-    picked = []
-    for _ in range(3):
-        picked.append(cups.pop(index))
-        index %= len(cups)
-    return picked
+    return first_cup, cups
 
 
-def getOrder(cups: List[int]) -> str:
-    n, one = len(cups), cups.index(1)
-    return ''.join(str(cups[(i+1) % n]) for i in range(one, one+n-1))
+def makeMoves(first_cup: ListNode, cups: dict, num_moves: int) -> ListNode:
+    num_cups = len(cups)
+    current_cup = first_cup
+
+    for _ in range(num_moves):
+        label = current_cup.label
+
+        # remove three cups from the circle
+        picked_cups = pickThreeCups(current_cup)
+        current_cup.next = picked_cups[-1].next
+
+        # select destination cup
+        dest = label - 1 or num_cups
+        while dest in (cup.label for cup in picked_cups):
+            dest = dest - 1 or num_cups
+        dest_cup = cups[dest]
+
+        # place picked cups clockwise of destination cup
+        picked_cups[-1].next = dest_cup.next
+        dest_cup.next = picked_cups[0]
+
+        current_cup = current_cup.next
+
+    return cups[1]
+
+
+def pickThreeCups(node: ListNode) -> Tuple[ListNode, ...]:
+    pick1 = node.next
+    pick2 = pick1.next
+    pick3 = pick2.next
+    return (pick1, pick2, pick3)
 
 
 if __name__ == '__main__':
