8cf2f5cf6f7ab8270a2dfa0ce146442aa9bd5cf5
Rajit Banerjee <rajit.banerjee@ucdconnect.ie>
2020-12-25 14:14:49 +0000

    simplify functions

--- 2020/day25-combo-breaker/crypto.py
+++ 2020/day25-combo-breaker/crypto.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 
-from typing import Tuple
-
 
 def main() -> None:
     with open('day25.in') as f:
@@ -10,22 +8,17 @@ def main() -> None:
     print(f"Part 1: {getEncryptionKey(card_pub_key, door_pub_key)}")
 
 
-def getEncryptionKey(card_pub_key: int, door_pub_key: int) -> int:
-    _, loop_size = transform(key=card_pub_key)
-    encrytpion_key, _ = transform(subject=door_pub_key, loop=loop_size)
-    return encrytpion_key
+def getEncryptionKey(*public_keys: int, value: int = 1) -> int:
+    for _ in range(getLoopSize(public_keys[0])):
+        value = (value * public_keys[1]) % 20201227
+    return value
 
 
-def transform(subject: int = 7, value: int = 1, key: int = 0,
-              loop: int = 0) -> Tuple[int, int]:
-    if loop:
-        for i in range(loop):
-            value = (value * subject) % 20201227
-    else:
-        while value != key:
-            loop += 1
-            value = (value * subject) % 20201227
-    return (value, loop)
+def getLoopSize(key: int, value: int = 1, subject: int = 7) -> int:
+    loop = 0
+    while value != key:
+        loop, value = loop + 1, (value * subject) % 20201227
+    return loop
 
 
 if __name__ == '__main__':
