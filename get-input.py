d91c0e06df42d7e3b7c32890d927f4de2cbfc41f
Rajit Banerjee <rajit.banerjee@ucdconnect.ie>
2020-12-31 13:26:29 +0000

    fix input download script

--- get-input.py
+++ get-input.py
@@ -5,13 +5,15 @@ Usage
 
 1. Make directory containing day number, e.g. `./2020/day<day>-<problem-name>`
 2. Run `$ ./get-input 2020` to download all input so far for given year.
-3. Alternatively, run script without arguments `$ ./get-input` to download the current year's input by default.
+3. Alternatively, run script without arguments `$ ./get-input` to download \
+the current year's input by default.
 
 """
 
 from datetime import datetime
 from pathlib import Path
 import os
+import re
 import sys
 
 import requests
@@ -22,8 +24,9 @@ SESSION_COOKIE = os.environ['AOC_COOKIE']
 
 def getInput(year, day):
     try:
+        regex = re.compile(rf'day(0?){day}-')
         for d in Path(f'./{year}/').iterdir():
-            if str(day) in str(d)[4:]:
+            if regex.search(str(d)):
                 folder = d
                 break
         outpath = folder/Path(f'day{day}.in')
@@ -41,7 +44,9 @@ def getInput(year, day):
             print(f'Input file already exists: {outpath}')
 
     except:
-        print(f'{year}, day {day}: Error! Check if cookie is valid, input is available, and problem folder has been created.')
+        msg = f'{year}, day {day}: Error! Check if cookie is valid, ' + \
+            'input is available, and problem folder has been created.'
+        print(msg)
 
 
 if __name__ == '__main__':
